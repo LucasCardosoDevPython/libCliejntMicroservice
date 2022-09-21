77b1c28f943b9ec40b3847759b310352ae30724c
Lucas Santos <lucas.santos@phoebus.local>
2022-09-21 09:51:26 -0300

    first commit

---
+++ src/main/java/library/libClientMicroservice/LibClientMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package library.libClientMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibClientMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibClientMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/library/libClientMicroservice/LibClientMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package library.libClientMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibClientMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
